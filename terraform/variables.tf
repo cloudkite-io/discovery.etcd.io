709eccfb60e7291d48928c6852705583e516c02f
Eduar Tua <eduartua@gmail.com>
2019-10-01 20:55:19 -0500

    Fix typo in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,13 @@ variable "gcp" {
 }
 
 variable "reference_dev_data" {
-  description = "Especify if we are referring to the development data"
+  description = "Specify if we are referring to the development data"
   default = false
   type = bool
 }
 
 variable "reference_prod_data" {
-  description = "Especify if we are referring to the production data"
+  description = "Specify if we are referring to the production data"
   default = false
   type = bool
 }
