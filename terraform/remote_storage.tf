0221eb436b143329fa7bb768a5daae54a691bd85
Eduar Tua <eduartua@gmail.com>
2019-10-03 09:30:39 -0500

    Adjunting file layout to have terraform modules

--- terraform/remote_storage.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "${var.gcp["project"]}-infrastructure"
-    prefix  = "terraform/terraform.state"
-  }
-}
--- terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable environment {
-  type = "string"
-}
-
-variable "gcp" {
-  type = "map"
-}
